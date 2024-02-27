d669fc40eb11a13f4c1cf5d3ba07f2cc787aa232
milko <ph.watcharapol_st@tni.ac.th>
2024-02-27 18:18:59 +0700

    first commit

---
+++ app/src/main/java/win/notoshi/genesec/MainActivity.kt
@@ -0,0 +1,11 @@
+package win.notoshi.genesec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
